641a3f8cdc0531a7d00779cb048f5f233162555d
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-11 23:34:22 +0530

    This will refer to the object

--- core-js/this-keyword/this.js
+++ core-js/this-keyword/this.js
@@ -1,15 +1,11 @@
 // "this" keyword in js (Implicit Binding)
 // Explain this keyword
 
-class User {
-  constructor(n) {
-    this.name = n;
-  }
-
+const user = {
+  firstName: "John",
   getName() {
-    console.log(this.name);
-  }
-}
-
-const user1 = new User("john");
-user1.getName();
+    const firstName = "Tom";
+    return this.firstName;
+  },
+};
+console.log(user.getName());
