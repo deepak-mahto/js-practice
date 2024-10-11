9618841444baf6ab1d3a0a0784ad1ef299e57e09
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-11 23:29:25 +0530

    Use of this in class

--- core-js/this-keyword/this.js
+++ core-js/this-keyword/this.js
@@ -1,28 +1,15 @@
 // "this" keyword in js (Implicit Binding)
 // Explain this keyword
 
-this.a = 24;
-console.log(this);
-console.log(typeof this);
-console.log(this.a);
+class User {
+  constructor(n) {
+    this.name = n;
+  }
 
-function getParam() {
-  const obj = {
-    a: 23,
-    fn() {
-      console.log(this);
-      console.log(this.a);
-    },
-    childObj: {
-      child: 11,
-      getDetails() {
-        console.log(`${this.a} and ${this.child}`);
-      },
-    },
-  };
-  obj.fn();
-  console.log(this.a);
-  console.log(this);
+  getName() {
+    console.log(this.name);
+  }
 }
 
-getParam();
+const user1 = new User("john");
+user1.getName();
